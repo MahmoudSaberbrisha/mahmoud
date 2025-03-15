b8e67f172ac239e6e4ff273e378da247afb74700
ma7moudsaber-oss <mido10sp@gmail.com>
2025-03-15 23:46:28 +0200

    first commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,8 @@
     @extends('admin.masterpage')
     @section('content')
         <center>
-            <Welcome style="font-size: 60px">Welcome Back MR {{ Auth::guard('admin')->user()->name }} In Admin Dashboard
+            <Welcome style="font-size: 60px"> حسبنا الله ونعم الوكيل فيكم كلكم..........
+                {{ Auth::guard('admin')->user()->name }} يقول
             </Welcome>
         </center>
     @endsection
